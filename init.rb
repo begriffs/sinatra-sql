e2dac3fd1ebccf5a5735c0d9b84b316cf1fb409b
Joe Nelson <cred+github@begriffs.com>
2012-06-29 11:13:59 -0500

    favicon

--- init.rb
+++ init.rb
@@ -3,6 +3,8 @@ require './db/config.rb'
 configure do
   opts = DB::Config[settings.environment]
   $db  = PG::Connection.new opts if opts.has_key? 'dbname'
+
+  set :public_folder, 'public'
 end
 
 def sql cmd, *args
