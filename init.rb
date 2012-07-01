37ecb301dbcf3958e00e91e60c1d9fd85f205a29
Joe Nelson <cred+github@begriffs.com>
2012-07-01 17:31:11 -0500

    parse ENV["DATABASE_URL"] for pg gem

--- init.rb
+++ init.rb
@@ -1,9 +1,17 @@
 begin; require './db/config.rb'; rescue LoadError; end
+require 'uri'
 
 configure do
-  opts = ENV['DATABASE_URL'] || DB::Config[settings.environment.to_s]
-  $db  = PG::Connection.new opts
-
+  uri = URI.parse ENV['DATABASE_URL']
+  if uri
+    opts = {
+      'hostname' => uri.host
+      'dbname'   => uri.path.split('/')[1]
+    }
+  else
+    opts = DB::Config[settings.environment.to_s]
+  end
+  $db = PG::Connection.new opts
   set :public_folder, 'public'
 end
 
